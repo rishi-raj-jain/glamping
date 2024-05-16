f03fe6d0ece38da806277524d3595266c507da98
mewdev <michal.wierzgon@gmail.com>
2024-05-16 15:45:45 +0200

    updating

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,41 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		colors: {
-			transparent: 'transparent',
-			current: 'currentColor',
-			'white': '#FFFFFF',
-			'dark-green': '#1F3832',
-			'glamping-light-green': '#C8E8C3',
-			'primary-light': '##F3EDDF',
-			'hover-color': '#8C8C8C',
-		  },
-	  extend: {
-		animation: {
-			marquee: 'marquee 80s linear infinite',
-			marquee2: 'marquee2 80s linear infinite',
-			rotate: 'spin 10s linear infinite',
+	extend: {
+	  colors: {
+		'dark-green': '#1F3832',
+		'glamping-light-green': '#C8E8C3',
+		'primary-light': '#F3EDDF',
+		'hover-color': '#8C8C8C',
+	  },
+	  animation: {
+		marquee: 'marquee 80s linear infinite',
+		marquee2: 'marquee2 80s linear infinite',
+		rotate: 'spin 10s linear infinite',
+	  },
+	  keyframes: {
+		marquee: {
+		  '0%': { transform: 'translateX(0%)' },
+		  '100%': { transform: 'translateX(-100%)' },
 		},
-		keyframes: {
-			marquee: {
-				'0%': { transform: 'translateX(0%)' },
-				'100%': { transform: 'translateX(-100%)' },
-			  },
-			  marquee2: {
-				'0%': { transform: 'translateX(100%)' },
-				'100%': { transform: 'translateX(0%)' },
-			  },
-		  spin: {
-			'0%': { transform: 'rotate(0deg)' },
-			'100%': { transform: 'rotate(360deg)' },
-		  },
+		marquee2: {
+		  '0%': { transform: 'translateX(100%)' },
+		  '100%': { transform: 'translateX(0%)' },
+		},
+		spin: {
+		  '0%': { transform: 'rotate(0deg)' },
+		  '100%': { transform: 'rotate(360deg)' },
 		},
 	  },
 	},
 	plugins: ["prettier-plugin-tailwindcss"],
   }
-  
 
 // DEFAULT CONFIG FILE
 
